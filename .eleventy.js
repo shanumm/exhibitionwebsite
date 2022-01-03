4ad3a264104c791a08eb5c18dcaa78d65473c7ba
Pushkar Mishra <35146129+shanumm@users.noreply.github.com>
2022-01-03 17:47:06 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/static");
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/admin");
 
 eleventyConfig.addFilter("postDate",(dataObj) =>{
   return DateTime.fromJSDate(dataObj).toLocaleString(DateTime.DATE_MED);
