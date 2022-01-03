aad4ba525d834f7d0c31371d53f8154206b1909f
shanumm <shanumishra199@gmail.com>
2022-01-03 17:25:07 +0530

    commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const {DateTime} = require("luxon");
+
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/static");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+eleventyConfig.addFilter("postDate",(dataObj) =>{
+  return DateTime.fromJSDate(dataObj).toLocaleString(DateTime.DATE_MED);
+})
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
